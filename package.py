75d6ad215a1e2f317cb0a06f359e874c4ab31ce3
Steven <evanssk29@gmail.com>
2023-10-03 12:39:11 -0400

    submission 2

--- package.py
+++ package.py
@@ -47,10 +47,4 @@ class Package:
                 state = "UT"
                 zip = "84103"
 
-            # else :
-            #     address = "410 S State St"
-            #     city = "Salt Lake City"
-            #     state = "UT"
-            #     zip = "84111"
-
             return "Package ID: %s, Address: %s, %s, %s, %s, Deadline: %s, Weight: %s lbs, Status: %s, Delivered: %s, Time left HUB: %s" % (self.id, address, city, state, zip, self.deadline, self.weight, new_status, self.delivery_time, self.left_hub)
